2bce83763808ba2a1c65b72daf51fc6221640408
Choudhury555 <choudhurypanda143@gmail.com>
2025-05-01 00:53:12 +0530

    Java Revision Started

---
+++ PRACTICE JAVA/Planet.java
@@ -0,0 +1,9 @@
+class Planet{
+    public static void revolve(){
+        System.out.println("Revolve");
+    }
+    public static void main(String[] args) {
+        Planet earth = new Planet();
+        earth.revolve();
+    }
+}
