df1eb0371891ad18858de21ff0931a3ecf8717ef
Choudhury555 <choudhurypanda143@gmail.com>
2025-05-01 02:21:57 +0530

    Java Revision Started

---
+++ JAVA_PRACTICE/src/Planet.java
@@ -0,0 +1,9 @@
+public class Planet {
+    public void revolve(){
+        System.out.println("Revolve");
+    }
+    public static void main(String[] args) {
+        Planet earth = new Planet();
+        earth.revolve();
+    }
+}
